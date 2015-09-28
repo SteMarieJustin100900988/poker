4debc0992ce9385bc4022e55b8f28146fd4d02af
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-27 21:21:27 -0400

    passes test
    Also fixed string comparison from == to .equals()

--- poker/src/poker/Deck.java
+++ poker/src/poker/Deck.java
@@ -4,65 +4,73 @@ import java.util.ArrayList;
 
 public class Deck {
 	ArrayList<Card> cards;
+	ArrayList<Card> validCards;
 
 	public Deck(){
-		cards = new ArrayList<Card>();
+		cards = basicDeck();
+		validCards = basicDeck();
+	}
+	
+	private ArrayList<Card> basicDeck(){
+		ArrayList<Card> temp = new ArrayList<Card>();
 		//spades
-		cards.add(new Card("ace", "Spade"));
-		cards.add(new Card("two", "Spade"));
-		cards.add(new Card("three", "Spade"));
-		cards.add(new Card("four", "Spade"));
-		cards.add(new Card("five", "Spade"));
-		cards.add(new Card("six", "Spade"));
-		cards.add(new Card("seven", "Spade"));
-		cards.add(new Card("eight", "Spade"));
-		cards.add(new Card("nine", "Spade"));
-		cards.add(new Card("ten", "Spade"));
-		cards.add(new Card("jack", "Spade"));
-		cards.add(new Card("queen", "Spade"));
-		cards.add(new Card("king", "Spade"));
+		temp.add(new Card("ace", "Spade"));
+		temp.add(new Card("two", "Spade"));
+		temp.add(new Card("three", "Spade"));
+		temp.add(new Card("four", "Spade"));
+		temp.add(new Card("five", "Spade"));
+		temp.add(new Card("six", "Spade"));
+		temp.add(new Card("seven", "Spade"));
+		temp.add(new Card("eight", "Spade"));
+		temp.add(new Card("nine", "Spade"));
+		temp.add(new Card("ten", "Spade"));
+		temp.add(new Card("jack", "Spade"));
+		temp.add(new Card("queen", "Spade"));
+		temp.add(new Card("king", "Spade"));
 		//clubs
-		cards.add(new Card("ace", "Club"));
-		cards.add(new Card("two", "Club"));
-		cards.add(new Card("three", "Club"));
-		cards.add(new Card("four", "Club"));
-		cards.add(new Card("five", "Club"));
-		cards.add(new Card("six", "Club"));
-		cards.add(new Card("seven", "Club"));
-		cards.add(new Card("eight", "Club"));
-		cards.add(new Card("nine", "Club"));
-		cards.add(new Card("ten", "Club"));
-		cards.add(new Card("jack", "Club"));
-		cards.add(new Card("queen", "Club"));
-		cards.add(new Card("king", "Club"));
+		temp.add(new Card("ace", "Club"));
+		temp.add(new Card("two", "Club"));
+		temp.add(new Card("three", "Club"));
+		temp.add(new Card("four", "Club"));
+		temp.add(new Card("five", "Club"));
+		temp.add(new Card("six", "Club"));
+		temp.add(new Card("seven", "Club"));
+		temp.add(new Card("eight", "Club"));
+		temp.add(new Card("nine", "Club"));
+		temp.add(new Card("ten", "Club"));
+		temp.add(new Card("jack", "Club"));
+		temp.add(new Card("queen", "Club"));
+		temp.add(new Card("king", "Club"));
 		//Heart
-		cards.add(new Card("ace", "Heart"));
-		cards.add(new Card("two", "Heart"));
-		cards.add(new Card("three", "Heart"));
-		cards.add(new Card("four", "Heart"));
-		cards.add(new Card("five", "Heart"));
-		cards.add(new Card("six", "Heart"));
-		cards.add(new Card("seven", "Heart"));
-		cards.add(new Card("eight", "Heart"));
-		cards.add(new Card("nine", "Heart"));
-		cards.add(new Card("ten", "Heart"));
-		cards.add(new Card("jack", "Heart"));
-		cards.add(new Card("queen", "Heart"));
-		cards.add(new Card("king", "Heart"));
+		temp.add(new Card("ace", "Heart"));
+		temp.add(new Card("two", "Heart"));
+		temp.add(new Card("three", "Heart"));
+		temp.add(new Card("four", "Heart"));
+		temp.add(new Card("five", "Heart"));
+		temp.add(new Card("six", "Heart"));
+		temp.add(new Card("seven", "Heart"));
+		temp.add(new Card("eight", "Heart"));
+		temp.add(new Card("nine", "Heart"));
+		temp.add(new Card("ten", "Heart"));
+		temp.add(new Card("jack", "Heart"));
+		temp.add(new Card("queen", "Heart"));
+		temp.add(new Card("king", "Heart"));
 		//Diamond
-		cards.add(new Card("ace", "Diamond"));
-		cards.add(new Card("two", "Diamond"));
-		cards.add(new Card("three", "Diamond"));
-		cards.add(new Card("four", "Diamond"));
-		cards.add(new Card("five", "Diamond"));
-		cards.add(new Card("six", "Diamond"));
-		cards.add(new Card("seven", "Diamond"));
-		cards.add(new Card("eight", "Diamond"));
-		cards.add(new Card("nine", "Diamond"));
-		cards.add(new Card("ten", "Diamond"));
-		cards.add(new Card("jack", "Diamond"));
-		cards.add(new Card("queen", "Diamond"));
-		cards.add(new Card("king", "Diamond"));
+		temp.add(new Card("ace", "Diamond"));
+		temp.add(new Card("two", "Diamond"));
+		temp.add(new Card("three", "Diamond"));
+		temp.add(new Card("four", "Diamond"));
+		temp.add(new Card("five", "Diamond"));
+		temp.add(new Card("six", "Diamond"));
+		temp.add(new Card("seven", "Diamond"));
+		temp.add(new Card("eight", "Diamond"));
+		temp.add(new Card("nine", "Diamond"));
+		temp.add(new Card("ten", "Diamond"));
+		temp.add(new Card("jack", "Diamond"));
+		temp.add(new Card("queen", "Diamond"));
+		temp.add(new Card("king", "Diamond"));
+		
+		return temp;
 	}
 	
 	public int remainingCards(){
@@ -76,14 +84,25 @@ public class Deck {
 	}
 	
 	public void returnCard(Card c){
-		if (!cardInDeck(c)){
-			cards.add(c);
+		if(validCard(c)){
+			if (!cardInDeck(c)){
+				cards.add(c);
+			}
 		}
 	}
 	
 	public boolean cardInDeck(Card c){
-		for(int i=0; i < remainingCards(); i++){
-			if(c.getValue() == cards.get(i).getValue()){
+		for(int i=0; i < cards.size(); i++){
+			if(c.getValue().equals(cards.get(i).getValue())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean validCard(Card c){
+		for(int i=0; i < validCards.size(); i++){
+			if(c.getValue().equals(validCards.get(i).getValue())){
 				return true;
 			}
 		}
