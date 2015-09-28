2057e4023833a05a31e925530c9b5f12a3b57c2a
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-28 04:39:08 -0400

    passes test

--- poker/src/poker/Game.java
+++ poker/src/poker/Game.java
@@ -1,12 +1,18 @@
 package poker;
 
+import java.util.ArrayList;
+
 public class Game {
+	ArrayList<Player> players;
 
 	public Game(int playerNum){
-		
+		players = new ArrayList<Player>();
+		for(int i=0; i<playerNum; i++){
+			players.add(new Player());
+		}
 	}
 	
 	public String getPlayerID(int playerN){
-		return "";
+		return players.get(playerN).getPlayerID();
 	}
 }
