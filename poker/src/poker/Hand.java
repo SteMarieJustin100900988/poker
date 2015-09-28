7340f0ee105729afc6ad00300fd59b44a409e4f6
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-27 23:33:18 -0400

    passes test
    Wrote hand comparison for exact matches.

--- poker/src/poker/Hand.java
+++ poker/src/poker/Hand.java
@@ -1,5 +1,7 @@
 package poker;
 
+import java.util.ArrayList;
+
 public class Hand {
 	private Card c1, c2, c3, c4, c5;
 	
@@ -43,6 +45,37 @@ public class Hand {
 	
 	//compares 2 hands to see if their rank is equal
 	public boolean compare(Hand h){
-		return false;
+		ArrayList<Integer> ignore = new ArrayList<Integer>();
+		int t;
+		for(int i=0; i<5; i++){
+			t = compareCard(ignore, h, i);
+			if(t>=0){
+				ignore.add(t);
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	//compares one card in hand to the entire other hand
+	//while ignoring the other hand's positions that are in al
+	private int compareCard(ArrayList<Integer> al, Hand h, int c){
+		int r = -1;
+		for (int i=0; i<5; i++){
+			for(int j=0; j<al.size(); j++){
+				if(i == al.get(j)){
+					i++;
+					break;
+				}
+			}
+			if(i>4){
+				break;
+			}
+			if(getCard(c).equals(h.getCard(i))){
+				r = i;
+			}
+		}
+		return r;
 	}
 }
