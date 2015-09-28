17452faa18a3fc01032ae3f8874551cbf146571d
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-27 20:17:08 -0400

    More tests

--- poker/src/poker/getCardValueFromCard.java
+++ poker/src/poker/getCardValueFromCard.java
@@ -9,7 +9,11 @@ public class getCardValueFromCard {
 	@Test
 	public void test() {
 		Card c = new Card("four", "Club");
+		Card c1 = new Card("five", "Diamond");
 		assertEquals( "fourClub", c.getValue());
+		assertNotEquals( "fiveDiamond", c.getValue());
+		assertEquals( "fiveDiamond", c1.getValue());
+		assertNotEquals( "fourClub", c1.getValue());
 	}
 
 }
