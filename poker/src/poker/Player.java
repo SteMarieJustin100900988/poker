38bfeed7e3e2797f59c53b454255a92969658417
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-28 04:26:17 -0400

    passes test

--- poker/src/poker/Player.java
+++ poker/src/poker/Player.java
@@ -1,12 +1,15 @@
 package poker;
 
 public class Player {
+	static int initID = 0;
+	String pid;
 
 	public Player(){
-		
+		pid = "Player " + initID;
+		initID++;
 	}
 	
 	public String getPlayerID(){
-		return "";
+		return pid;
 	}
 }
