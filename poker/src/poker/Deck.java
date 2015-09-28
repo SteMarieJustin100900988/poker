91bf6793be41e7c78cb850a1ecd0340d68560c17
SteMarieJustin100900988 <justin.stemarie@carleton.ca>
2015-09-27 20:41:44 -0400

    passes test

--- poker/src/poker/Deck.java
+++ poker/src/poker/Deck.java
@@ -1,8 +1,71 @@
 package poker;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+	ArrayList<Card> cards;
+
+	public Deck(){
+		cards = new ArrayList<Card>();
+		//spades
+		cards.add(new Card("ace", "Spade"));
+		cards.add(new Card("two", "Spade"));
+		cards.add(new Card("three", "Spade"));
+		cards.add(new Card("four", "Spade"));
+		cards.add(new Card("five", "Spade"));
+		cards.add(new Card("six", "Spade"));
+		cards.add(new Card("seven", "Spade"));
+		cards.add(new Card("eight", "Spade"));
+		cards.add(new Card("nine", "Spade"));
+		cards.add(new Card("ten", "Spade"));
+		cards.add(new Card("jack", "Spade"));
+		cards.add(new Card("queen", "Spade"));
+		cards.add(new Card("king", "Spade"));
+		//clubs
+		cards.add(new Card("ace", "Club"));
+		cards.add(new Card("two", "Club"));
+		cards.add(new Card("three", "Club"));
+		cards.add(new Card("four", "Club"));
+		cards.add(new Card("five", "Club"));
+		cards.add(new Card("six", "Club"));
+		cards.add(new Card("seven", "Club"));
+		cards.add(new Card("eight", "Club"));
+		cards.add(new Card("nine", "Club"));
+		cards.add(new Card("ten", "Club"));
+		cards.add(new Card("jack", "Club"));
+		cards.add(new Card("queen", "Club"));
+		cards.add(new Card("king", "Club"));
+		//Heart
+		cards.add(new Card("ace", "Heart"));
+		cards.add(new Card("two", "Heart"));
+		cards.add(new Card("three", "Heart"));
+		cards.add(new Card("four", "Heart"));
+		cards.add(new Card("five", "Heart"));
+		cards.add(new Card("six", "Heart"));
+		cards.add(new Card("seven", "Heart"));
+		cards.add(new Card("eight", "Heart"));
+		cards.add(new Card("nine", "Heart"));
+		cards.add(new Card("ten", "Heart"));
+		cards.add(new Card("jack", "Heart"));
+		cards.add(new Card("queen", "Heart"));
+		cards.add(new Card("king", "Heart"));
+		//Diamond
+		cards.add(new Card("ace", "Diamond"));
+		cards.add(new Card("two", "Diamond"));
+		cards.add(new Card("three", "Diamond"));
+		cards.add(new Card("four", "Diamond"));
+		cards.add(new Card("five", "Diamond"));
+		cards.add(new Card("six", "Diamond"));
+		cards.add(new Card("seven", "Diamond"));
+		cards.add(new Card("eight", "Diamond"));
+		cards.add(new Card("nine", "Diamond"));
+		cards.add(new Card("ten", "Diamond"));
+		cards.add(new Card("jack", "Diamond"));
+		cards.add(new Card("queen", "Diamond"));
+		cards.add(new Card("king", "Diamond"));
+	}
+	
 	public int remainingCards(){
-		return 0;
+		return cards.size();
 	}
 }
